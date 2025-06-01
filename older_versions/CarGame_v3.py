e8d1579dce6a9e7fa42c6c991b315d9dff94f33a
NathanTaskerSchool <njtasker@outlook.com>
2025-06-01 23:44:34 +1200

    v4

--- older_versions/CarGame_v3.py
+++ older_versions/CarGame_v3.py
@@ -4,7 +4,7 @@ from Background_v3 import \
     Background  # Assuming Background_v3.py is in the same directory or Python path
 
 # Constants for Car
-CAR_IMAGE_PATH = 'assets/images/car_1.png'
+CAR_IMAGE_PATH = '../assets/images/car_1.png'
 CAR_SCALE_FACTOR = 0.2  # Adjust this to scale the car image appropriately
 # Car physics constants (world units)
 CAR_MAX_SPEED_FORWARD = 5.0  # world units / sec
@@ -167,7 +167,8 @@ def run_game():
     pygame.font.init()
 
     try:
-        game_icon = pygame.image.load('assets/images/game_icon.png')
+        game_icon = pygame.image.load(
+            '../assets/images/game_icon.png')
         pygame.display.set_icon(game_icon)
     except pygame.error as e:
         print(f"Error loading game icon: assets/images/game_icon.png")
